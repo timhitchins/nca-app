30c9450f6256bc5d197b31d53560b0c1685ecfbd
isnardo <isnardo.gandarilla.com>
2020-03-13 13:56:16 -0700

    add and fix on App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>hi tim</p>
+        <p>hi tim!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
