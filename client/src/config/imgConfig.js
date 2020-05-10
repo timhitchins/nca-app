108eae2b87d6f983370381bff9d5f5cc4e112472
Paige Williams <paige.williams@aclima.io>
2020-05-10 15:00:48 -0700

    begin adding content and styles

--- client/src/config/imgConfig.js
+++ client/src/config/imgConfig.js
@@ -3,36 +3,53 @@ export const imageConfig = [
     imageURI:
       "https://nca-toolkit.s3-us-west-2.amazonaws.com/OregonLive_hazepdxjpg-e8d26d8245f61043.jpg",
     altText: "person running",
+    heading: "Oregon has toxic air pollution",
+    body:
+      "<strong>We all have the right to breathe clean air.</strong> However, in the Portland metro area, the presence of fine and ultra-fine particulate matter from older dirty diesel engines makes our air unhealthy. <strong>Clackamas, Multnomah, and Washington counties rank in the top 5 percent of all counties nationwide for ambient diesel particulate concentrations</strong> and have the highest exposure rate of all counties in Oregon.",
   },
   {
     imageURI:
       "https://nca-toolkit.s3-us-west-2.amazonaws.com/parking+construction+no+copyright.jpg",
     altText: "construction site",
+    heading: "Diesel engines are a primary source of pollution",
+    body:
+      "Fine particulate matter, sometimes called <strong>PM2.5 or PM10</strong>, are tiny particles in the air that can cause health issues in humans if levels are high.<br><br><strong>Diesel engines are some of the worst emitters of fine particulate matter.</strong> Vehicles with diesel engines can be running in our neighborhoods, near our homes, schools, and places we work. They are especially prevalent around construction sites.",
   },
   {
     imageURI:
       "https://nca-toolkit.s3-us-west-2.amazonaws.com/parking+construction+no+copyright.jpg",
     altText: "construction site",
+    body:
+      "<strong>Exposure to diesel engine exhaust can cause cancer, increase the risk of heart attack, stroke and cardiovascular disease, cause adverse nervous system impacts, exacerbate asthma, and can lead to low-weight and preterm births.</strong>",
   },
   {
     imageURI:
       "https://nca-toolkit.s3-us-west-2.amazonaws.com/parking+construction+no+copyright.jpg",
     altText: "construction site",
+    body:
+      "In areas with higher diesel vehicle traffic, like near freight corridors, rail yards, or construction sites, levels of diesel pollution are over 10 times Oregon health standards for healthy air.<br/><strong>Off-road equipment, primarily construction equipment, is responsible for 65 percent of diesel particulate matter in the Portland area.</strong><br/>Off-road equipment is what we call the heavy construction vehicles such as excavators, wheel loaders, articulated and rigid dump trucks.",
   },
   {
     imageURI:
       "https://nca-toolkit.s3-us-west-2.amazonaws.com/parking+construction+no+copyright.jpg",
     altText: "construction site",
+    body:
+      "The main sources of toxic air pollution in Oregon are: diesel engines, industry, automobiles, burning wood (residential, commercial, wildfires).<br><strong>Diesel Engines produce particulate matter, including black carbon</strong>.About a third of all black carbon comes from off-road vehicles. Older diesel engines can emit 85% more black carbon than newer engines.<br><strong>In Oregon, these vehicles are not required to meet diesel exhaust standards.</strong>",
   },
   {
     imageURI:
       "https://nca-toolkit.s3-us-west-2.amazonaws.com/parking+construction+no+copyright.jpg",
     altText: "construction site",
+    heading: "Children are expecially vulnerable",
+    body:
+      "<strong>Children are effected by air pollution more than adults</strong> because their lungs are still in the developmental phase and they breathe, on average, 50 percent more air per pound of body weight than adults do.<br><strong>A 2014 Multnomah County Department of Health study found that that communities of color are exposed to levels of diesel pollution 2‐3 times higher than their white counterparts,</strong> with Black/African Americans facing the largest disparate exposure compared to whites. Rates of asthma in historically non-white neighborhoods are significantly higher than elsewhere.",
   },
   {
     imageURI:
       "https://nca-toolkit.s3-us-west-2.amazonaws.com/parking+construction+no+copyright.jpg",
     altText: "construction site",
+    body:
+      "Studies have shown that women exposed to high levels of air pollution in their third trimester were twice as likely to deliver children with autism.<br>Increasing evidence shows that air pollution affects a child’s brain development, lowering test scores and increasing of ADHD, ADD as well as impulse control.<br>Research into the effects of air pollution among the elderly suggest that it can raise the risk of strokes,  Parkinson’s disease, and Alzheimer’s disease.",
   },
   {
     imageURI:
