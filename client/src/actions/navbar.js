10cb24feb9cf75752cda8776af7968a5a5729e0c
Tim Hitchins <tim.x.hitchins@gmail.com>
2020-05-11 11:00:58 -0700

    add toggle nave page action and reducer state

--- client/src/actions/navbar.js
+++ client/src/actions/navbar.js
@@ -1,10 +1,20 @@
 export const TOGGLE_NAV_DRAWER = "TOGGLE_NAV_DRAWER";
+export const TOGGLE_NAV_PAGE = "TOGGLE_NAV_PAGE";
 
 export function togglenavDrawerAction(isOpen) {
   return {
     type: TOGGLE_NAV_DRAWER,
     payload: {
       isOpen,
+    },
+  };
+}
+
+export function toggleNavbarPageAction(page) {
+  return {
+    type: TOGGLE_NAV_PAGE,
+    payload: {
+      page,
     },
   };
 }
