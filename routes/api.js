2c335181523e0abd549b8624bcae135e0dc9333e
Tim Hitchins <tim.x.hitchins@gmail.com>
2020-05-31 15:50:11 -0700

    add PDI algo to new fucntion, generate PDI --in progress

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,4 @@
-import { createBuffer, createArcGISGeometry } from "../utils/dataUtils";
+import { createBuffer, createArcGISGeometry, addPDIToFeatures } from "../utils/dataUtils";
 import { fetchPermitData } from "../utils/fetchUtils";
 import Router from "express-promise-router";
 export const router = new Router();
@@ -6,17 +6,23 @@ export const router = new Router();
 // Example URI
 // http://localhost:5000/api/location//%7B%22lon%22:-122.643154,%22lat%22:45.55659%7D/1/miles
 router.get("/:coords/:radius/:units", async (req, res) => {
-    // "/:coords/:radius/:units"
+  // "/:coords/:radius/:units"
   const coords = JSON.parse(req.params.coords);
   const { radius, units } = req.params;
 
   // create the search feature
   const searchBuffer = createBuffer(coords, radius, units);
   const arcGeometryObject = JSON.stringify(createArcGISGeometry(searchBuffer));
-
   // await the feature service fetch
-  //errors return a error message object
-  const data = await fetchPermitData(arcGeometryObject);
+  //errors return an error message object
+  try {
+    const data = await fetchPermitData(arcGeometryObject);
+    const pdiData = addPDIToFeatures(data)
 
-  res.json(data);
+    // new data here
+    res.json(pdiData);
+  } catch (err) {
+    res.json({ message: err });
+  }
+
 });
--- utils/dataUtils.js
+++ utils/dataUtils.js
@@ -15,3 +15,101 @@ export function createBuffer(coords, radius, units) {
 export function createArcGISGeometry(searchBuffer) {
   return geojsonToArcGIS(searchBuffer);
 }
+
+//create the new
+export function addPDIToFeatures(inData) {
+  const pdiDataFeatures = inData.features.map((feature) => {
+    let demo, stat, sqFoot, stor;
+    if (feature.properties.WORK_DESCRIPTION === "New Construction") {
+
+      const { STATUS, TOTALSQFT, NUMBSTORIES } = feature.properties;
+      //status
+      if (STATUS === "Under Inspection") {
+        stat = 1;
+      } else if (STATUS === "Under Review" || STATUS === "Issued") {
+        stat = 3;
+      } else {
+        stat = 0;
+      }
+      // total sq ft
+      if (TOTALSQFT < 10000) {
+        sqFoot = 1;
+      } else if (TOTALSQFT >= 10000 && TOTALSQFT < 50000) {
+        sqFoot = 2;
+      } else if (TOTALSQFT >= 50000 && TOTALSQFT < 100000) {
+        sqFoot = 3;
+      } else if (TOTALSQFT >= 100000) {
+        sqFoot = 4;
+      } else {
+        sqFoot = 0;
+      }
+      // number of stores
+      if (NUMBSTORIES < 1) {
+        stor = 1;
+      } else if (NUMBSTORIES >= 3 && NUMBSTORIES < 5) {
+        stor = 2;
+      } else if (NUMBSTORIES >= 3) {
+        stor = 3;
+      } else {
+        stor = 0;
+      }
+
+    // temporary demo val is 1
+    demo = 1;
+    }
+    //calculate PDI
+    const PDI = demo + stat + sqFoot + stor;
+    feature.properties.PDI = PDI;
+    return feature;
+  });
+
+  inData.features = pdiDataFeatures;
+  return inData;
+}
+
+// IF WORK = “New Construction”
+
+// If
+// STATE_ID has a duplicate / appears more than once
+// Then Demo = 3 [DEMOLITION IS OCCURING]
+// If
+// 	STATE_ID does not appear more than once
+// 	Then Demo = 1 [NO DEMOLTION]
+
+// If
+// 	STATUS = “Under Inspection”
+// 	Then Stat=1
+
+// If
+// 	STATUS = “Under Review” OR STATUS = “Issued”
+// 	Then Stat=3
+
+// If
+// 	TOTAL_SQFT < 10,000
+// 	Then SqFoot=1
+
+// If
+// 	TOTAL_SQFT >= 10,000 AND TOTAL_SQFT < 50,000
+// 	Then SqFoot=2
+
+// If
+// 	TOTAL_SQFT >= 50,000 AND TOTAL_SQFT < 100,000
+// 	Then SqFoot=3
+
+// If
+// 	TOTAL_SQFT >= 100,000
+// 	Then SqFoot=4
+
+// If
+// 	STORIES < 3
+// 	Then Stor=1
+
+// If
+// 	STORIES >= 3 AND STORIES < 5
+// 	Then Stor=2
+
+// If
+// 	STORIES >= 5
+// 	Then Stor=3
+
+// PDI = Demo + Stat + SqFoot + Stor
