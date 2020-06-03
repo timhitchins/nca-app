08ed1884433f7e83d52cd5455f03f8a0477afdd3
Tim Hitchins <tim.x.hitchins@gmail.com>
2020-06-03 12:20:46 -0700

    add mapState action and reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import navbar from "./navbar";
-import slides from "./slides"
+import slides from "./slides";
+import mapState from "./mapState";
 import { combineReducers } from "redux";
 
 export default combineReducers({
   navbar,
-  slides
+  slides,
+  mapState,
 });
---
+++ client/src/reducers/mapState.js
@@ -0,0 +1,18 @@
+import { GET_MAP_STATE } from "../actions/mapState";
+
+const initialViewport = {
+  latitude: 42.40230199308517,
+  longitude: -83.11182404081912,
+  zoom: 10,
+  bearing: 0,
+  pitch: 0
+};
+
+export default function mapState(state = {}, action) {
+  switch (action.type) {
+    case GET_MAP_STATE:
+      return { ...state, ...action.payload };
+    default:
+      return state;
+  }
+}
