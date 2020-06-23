9a45930f655199259b2832821f120ff31833c860
Tim Hitchins <tim.x.hitchins@gmail.com>
2020-06-23 12:53:37 -0700

    commented out unused code, will remove later

--- client/src/components/map/Map/SiteMarkers.js
+++ client/src/components/map/Map/SiteMarkers.js
@@ -1,43 +1,43 @@
-import React, { PureComponent } from "react";
-import { Marker } from "react-map-gl";
-import PropTypes from "prop-types";
-import Pin from "./Pin";
+// import React, { PureComponent } from "react";
+// import { Marker } from "react-map-gl";
+// import PropTypes from "prop-types";
+// import Pin from "./Pin";
 
-const residentialMarkerURI =
-  "https://nca-toolkit.s3-us-west-2.amazonaws.com/noun_cottage_191764.png";
-const commercialMarkerURI =
-  "https://nca-toolkit.s3-us-west-2.amazonaws.com/noun_City_191844.png";
+// const residentialMarkerURI =
+//   "https://nca-toolkit.s3-us-west-2.amazonaws.com/noun_cottage_191764.png";
+// const commercialMarkerURI =
+//   "https://nca-toolkit.s3-us-west-2.amazonaws.com/noun_City_191844.png";
 
-const markerStyles = {
-  height: "75px",
-  width: "75px",
-  cursor: "pointer",
-};
+// const markerStyles = {
+//   height: "75px",
+//   width: "75px",
+//   cursor: "pointer",
+// };
 
-class SiteMarkers extends PureComponent {
-  static propTypes = {
-    markerSelector: PropTypes.object.isRequired,
-    dispatch: PropTypes.func.isRequired,
-  };
+// class SiteMarkers extends PureComponent {
+//   static propTypes = {
+//     markerSelector: PropTypes.object.isRequired,
+//     dispatch: PropTypes.func.isRequired,
+//   };
 
-  render() {
-    // geojson features
-    const { features } = this.props.mapData.siteMarkers;
-    return features.map((feature) => {
-      const [lon, lat] = feature.geometry.coordinates;
-      return (
-        <Marker
-          key={feature.id}
-          longitude={lon}
-          latitude={lat}
-          offsetTop={-25}
-          offsetLeft={-15}
-        >
-          <img src={residentialMarkerURI} style={markerStyles}></img>
-        </Marker>
-      );
-    });
-  }
-}
+//   render() {
+//     // geojson features
+//     const { features } = this.props.mapData.siteMarkers;
+//     return features.map((feature) => {
+//       const [lon, lat] = feature.geometry.coordinates;
+//       return (
+//         <Marker
+//           key={feature.id}
+//           longitude={lon}
+//           latitude={lat}
+//           offsetTop={-25}
+//           offsetLeft={-15}
+//         >
+//           <img src={residentialMarkerURI} style={markerStyles}></img>
+//         </Marker>
+//       );
+//     });
+//   }
+// }
 
-export default SiteMarkers;
+// export default SiteMarkers;
