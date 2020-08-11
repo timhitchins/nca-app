7f3d7757bf4437d0c68f71b0d17303dd109b3ce9
Tim Hitchins <tim.x.hitchins@gmail.com>
2020-08-11 16:20:03 -0700

    update modal state

--- client/src/components/map/Modal/MapModal.js
+++ client/src/components/map/Modal/MapModal.js
@@ -1,15 +1,41 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
-import propTypes from "prop-types";
+import { mobileModalImage, fullModalImage } from "../../../config/modalConfig";
 import "./MapModal.scss";
 
 class MapModal extends Component {
+  state = {
+    imgURL: null,
+  };
+
+  _setImage = () => {
+    const isMobile = window.matchMedia("(max-width: 768px)").matches;
+    const el = document.querySelector(".modal-content > img");
+
+    if (isMobile) {
+      this.setState({ imgURL: mobileModalImage });
+    } else {
+      this.setState({ imgURL: fullModalImage });
+    }
+  };
+
+  componentDidMount() {
+    this._setImage();
+
+    window.addEventListener("resize", () => {
+      this._setImage();
+    });
+  }
+
   render() {
+    const { imgURL } = this.state;
     return (
       <aside className="modal-container">
         <div className="modal-content">
+          <div className="modal-close">
+            <a href="#close">&#x274c;</a>
+          </div>
           <img
-            src="https://nca-toolkit.s3-us-west-2.amazonaws.com/modal_fullscreen.png"
+            src={imgURL}
             alt="An image with basic instructions for using the map"
           ></img>
         </div>
